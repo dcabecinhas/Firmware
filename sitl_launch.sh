70803232e9ff16eda268f082bdaf335cb64f284b
David Cabecinhas <dcabecinhas@isr.tecnico.ulisboa.pt>
2019-08-27 16:42:55 +0200

    Deal with <1 simulation time ratios

--- sitl_launch.sh
+++ sitl_launch.sh
@@ -4,6 +4,7 @@ if [ "$1" == "" ]; then
   echo "Example: $0 multi_uav_single_urdf_mavros_sitl_4.launch"
   exit 1
 fi
+# export PX4_SIM_SPEED_FACTOR=0.5
 DONT_RUN=1 make px4_sitl_default gazebo
 source ~/catkin_ws/devel/setup.bash    # (optional)
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
